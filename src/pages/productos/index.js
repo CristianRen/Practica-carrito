e6add8b57c395671a5762b599e3d709557dd1d0a
CristianRen <cristian-0405ra@hotmail.com>
2023-05-31 00:00:56 -0500

    Carrito-básico-react

---
+++ src/pages/productos/index.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { Alert } from "flowbite-react";
+import { ProductoCard } from "./components/productoCars";
+import { AlertaExito } from "../components/alertas";
+
+export default function Productos() {
+  const [productos, actualizarProductos] = useState(null);
+  const [alerta, actualizarAlerta] = useState(false);
+  useEffect(() => {
+    async function llamarProductos() {
+      const solicitud = await fetch("./productos.json");
+      const respuesta = await solicitud.json();
+
+      actualizarProductos(respuesta.productos);
+    }
+    llamarProductos();
+    console.log(productos);
+  }, []);
+
+  function agregarAlCarrito() {
+    actualizarAlerta(true);
+  }
+  return (
+    <div className="text-center">
+      {alerta ? (
+        <AlertaExito mensaje="Se ha enviado el producto al carrito"></AlertaExito>
+      ) : (
+        <></>
+      )}
+      <div className="flex flex-wrap justify-center ">
+        {productos ? (
+          productos.map((producto) => {
+            return (
+              <ProductoCard
+                producto={producto}
+                agregarAlCarrito={agregarAlCarrito}
+              ></ProductoCard>
+            );
+          })
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
+}
